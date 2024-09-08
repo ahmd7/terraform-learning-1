953ea25725c8e9cdf44552f6d11f331a57be25a8
Ahmed Samir <134155493+ahmd7@users.noreply.github.com>
2024-09-08 16:14:20 +0300

    what will happen

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 terraform {
-  required_version = ">= 0.12"
+  backend "remote" {
+    organization = "ahmed-samir"
+
+    workspaces {
+      name = "terraform-learning-1"
+    }
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws"{
+    region = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "tf-example"
+  }
 }
